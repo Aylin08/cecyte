030ece29b18d1f7b869b448fceb15ab1b2300f83
Aylin08 <75355673+Aylin08@users.noreply.github.com>
2022-11-06 09:11:41 -0800

    IMAGEN

--- components/Image.tsx
+++ components/Image.tsx
@@ -6,8 +6,8 @@ const Images = () => {
       style={{
         paddingTop: 30
       }}
-      src="https://raw.githubusercontent.com/Aylin08/cecyte/main/public/img/fondoc.jpg"
-      height={70}
+      src="https://raw.githubusercontent.com/Aylin08/cecyte/main/public/img/LOGO.jpg"
+      height={200}
       width={240}
 
     />
