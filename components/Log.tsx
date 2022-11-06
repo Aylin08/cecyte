c3d8c99cb3d3c4c1d940f4797a2779a458e397be
Aylin08 <75355673+Aylin08@users.noreply.github.com>
2022-11-06 08:52:44 -0800

    Login

--- components/Log.tsx
+++ components/Log.tsx
@@ -1,22 +1,66 @@
-import React from 'react'
-import { Image, Center, Text } from '@mantine/core';
+import React, { useState } from 'react'
+import { Container, Card, Text, Center, TextInput, PasswordInput, Group, Button } from '@mantine/core'
+import Link from 'next/link'
+import { BiUser, BiLock } from 'react-icons/bi'
+import { CgLogIn } from 'react-icons/cg'
+import Images from './Image';
+
+import { useRouter } from 'next/router'
 const Log = () => {
+
   return (
+    <Container size={400} px={0} >
+      <Card shadow="sm" p="lg" radius="md" withBorder
+        style={{
+          width: 350,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          height: 400,
+          position: 'absolute',
+          transform: 'translate(-50%, -50%)',
+          top: '50%',
+          left: '50%',
+        }}>
+        <Card.Section>
+          <Center>
+            {/*eslint-disable-next-line jsx-a11y/alt-text*/}
 
-    <Image
-      src='https://photos.app.goo.gl/UtE2cJ3Z5dwTh4kv7'
-      radius="sm"
-    >
-      <Center p="md">
-        <Text color="#000">
-          BackgroundImage component can be used to add any content on image. It is useful for hero
-          headers and other similar sections
-        </Text>
-      </Center>
-    </Image>
+          </Center>
+        </Card.Section>
+        <br></br>
+        <Text weight={700} align="center" style={{ fontFamily: 'Greycliff CF, sans-serif' }}>  Iniciar Sesión</Text>
+        <TextInput
+          style={{
+            paddingLeft: 30,
+            paddingRight: 20,
+            width: 300
+          }}
+          mt="sm"
+          label="Usuario"
+          /*placeholder="RPE"*/
+          icon={<BiUser></BiUser>}
 
-
+        />
+        <PasswordInput
+          style={{
+            paddingLeft: 30,
+            paddingRight: 20,
+            width: 300
+          }}
+          /** placeholder="Contraseña"*/
+          label="Contraseña"
+          icon={<BiLock></BiLock>}
+        />
+        <Group position="center" mt="md" mb="xs">
+          <br></br>
+          <Button leftIcon={<CgLogIn />} sx={(theme) => ({ backgroundColor: '#3F6D3F', '&:hover': { backgroundColor: theme.fn.darken('#A1C298', 0.05), }, })} type="submit" >
+            <Link href="/inicio">Iniciar Sesión</Link>
+          </Button>
+        </Group>
+      </Card>
+    </Container>
   )
 }
+
 
 export default Log
---
+++ components/appshel.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { AppShell, Text } from '@mantine/core';
+import Footers from './Footer';
+import Headers from './Headers';
+import NavBar from './Navbar';
+
+const AppShel = ({ children, tituloPagina }) => {
+
+  return (
+    <AppShell
+      styles={{
+        main: {
+          background: '#FFFFFF',
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      navbar={
+        <NavBar/>
+      }
+      footer={
+        <Footers />
+      }
+      header={
+        <Headers />
+      }
+    >
+      <Text align="center" color='#A1C298' size="xl" transform="uppercase">{tituloPagina}</Text>
+      {children}
+    </AppShell>
+  )
+}
+
+export default AppShel
+
+
+
