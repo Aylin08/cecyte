e341c45a657e10fe94378059adaaf2f20302efda
Aylin08 <75355673+Aylin08@users.noreply.github.com>
2022-12-28 12:50:57 -0800

    opciones

--- components/opciones.tsx
+++ components/opciones.tsx
@@ -40,8 +40,9 @@ const Opciones = () => {
                 <Card shadow="sm" p="lg" radius="md" withBorder>
                     <Card.Section>
                         <Image
-                            src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                            src="https://raw.githubusercontent.com/Aylin08/cecyte/main/public/img/reporte.png"
                             height={160}
+                            width={"100%"}
 
                         />
                     </Card.Section>
