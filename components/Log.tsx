490dbae8393279dce2622caf37ffc0ef51ae838a
Aylin08 <75355673+Aylin08@users.noreply.github.com>
2022-12-27 17:18:21 -0800

    ICONS

--- components/Log.tsx
+++ components/Log.tsx
@@ -1,8 +1,10 @@
-import { Container, Card, Flex, Text, Center, TextInput, PasswordInput, Group, Button } from '@mantine/core'
+import { Container, Flex, Text, Center, TextInput, PasswordInput, Group, Button } from '@mantine/core'
 import Link from 'next/link'
-import { BiUser, BiLock } from 'react-icons/bi'
+import { FaUser } from 'react-icons/fa'
 import { CgLogIn } from 'react-icons/cg'
-import Images from './Image';
+import ImagenInicio from './LogoInicio';
+import { IconPassword } from '@tabler/icons';
+import { BiLogInCircle } from 'react-icons/bi';
 
 const Log = () => {
 
@@ -10,7 +12,8 @@ const Log = () => {
     <Container size={350} px={0}
       style={{
         paddingTop: '10%',
-        paddingBottom: '10%'
+        paddingBottom: '10%',
+
       }}
 
     >
@@ -21,10 +24,13 @@ const Log = () => {
         gap="xl"
         justify="center"
         align="center"
-        direction="column" >
+        direction="column"
+        style={{
+          borderRadius: '15%',
+        }}>
 
         <Center>
-          <Images />
+          <ImagenInicio />
         </Center>
         <Text weight={700} align="center" fz="lg" style={{ fontFamily: 'Greycliff CF, sans-serif', color: 'white' }}>  Bienvenido (a)</Text>
         <TextInput
@@ -33,11 +39,13 @@ const Log = () => {
             paddingRight: '10%',
             width: '90%',
 
+
           }}
           mt="sm"
           label="Usuario"
           placeholder="No. Empleado"
-          icon={<BiUser></BiUser>}
+          icon={<FaUser color='#571719'></FaUser>}
+          color='white'
 
         />
         <PasswordInput
@@ -49,11 +57,11 @@ const Log = () => {
           }}
           placeholder="Contraseña"
           label="Contraseña"
-          icon={<BiLock></BiLock>}
+          icon={<IconPassword color='#571719'></IconPassword>}
         />
         <Group position="center" mt="md" mb="xs">
           <br></br>
-          <Button leftIcon={<CgLogIn />} sx={(theme) => ({ backgroundColor: '#571719', '&:hover': { backgroundColor: theme.fn.darken('#743C3E', 0.05), }, })} type="submit" >
+          <Button leftIcon={<BiLogInCircle />} sx={(theme) => ({ backgroundColor: '#571719', '&:hover': { backgroundColor: theme.fn.darken('#743C3E', 0.05), }, })} type="submit" >
             <Link href="/inicio">Iniciar Sesión</Link>
           </Button>
         </Group>
---
+++ components/LogoInicio.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Image } from '@mantine/core'
+const ImagenInicio = () => {
+    return (
+        <Image
+            style={{
+                paddingTop: '30%',
+                marginRight: '18%'
+
+            }}
+            src="https://raw.githubusercontent.com/Aylin08/cecyte/main/public/img/LOGO.png"
+            height='150%'
+            width='150%'
+
+        />
+    )
+}
+
+export default ImagenInicio
