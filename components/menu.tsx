8485318a002871e354ea83487f4f59f061fbee6a
Aylin08 <75355673+Aylin08@users.noreply.github.com>
2022-12-28 19:47:31 -0800

    cambios

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,6 +1,7 @@
-import { BackgroundImage, ScrollArea, Text } from '@mantine/core'
+import { BackgroundImage, Center, ScrollArea, Text } from '@mantine/core'
 import React from 'react'
 import Footers from './Footer'
+import Images from './Image'
 import Opciones from './opciones'
 
 const MenuOP
@@ -17,6 +18,7 @@ const MenuOP
             >
                 <ScrollArea style={{ height: '100%' }} type="never">
                     <Text weight={700} align="center" fz="lg" style={{ fontFamily: 'Greycliff CF, sans-serif', color: 'white', paddingTop: '4%' }}>  Hola amigo(a)</Text>
+                    <Center><Images /> </Center>
                     <Opciones />
                     <Footers />
                 </ScrollArea>
